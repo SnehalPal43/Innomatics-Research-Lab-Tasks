735355e095f367b1c6a3f73b7608453fcaf4bd9d
SnehalPal43 <palsnehal2005@gmail.com>
2026-05-10 12:07:53 +0530

    Delete core/embeddings.py

--- core/embeddings.py
+++
@@ -1,9 +0,0 @@
-from sentence_transformers import SentenceTransformer
-from core.config import settings
-
-class EmbeddingModel:
-    def __init__(self):
-        self.model = SentenceTransformer(settings.EMBEDDING_MODEL)
-
-    def encode(self, texts):
-        return self.model.encode(texts).tolist()
